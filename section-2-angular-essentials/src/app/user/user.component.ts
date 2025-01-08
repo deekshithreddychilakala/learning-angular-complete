72622dc4c49d82c9521a0890bbcc4d9dd703a8ff
Chilakala Venkata Deekshith Reddy <deekshithreddychilakala@gmail.com>
2025-01-09 00:49:17 +0530

    Section 2: Angular Essentials - Components, Templates, Services & More

---
+++ section-2-angular-essentials/src/app/user/user.component.ts
@@ -0,0 +1,40 @@
+import { Component, computed, EventEmitter, Input, input, Output, output } from '@angular/core';
+import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
+
+// type User={ id: string, name: string, avatar: string };
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>()
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // })
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+
+}
