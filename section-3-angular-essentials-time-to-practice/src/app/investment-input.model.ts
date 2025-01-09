fe35fe352dde270d6c2559483e0720350b80f835
Chilakala Venkata Deekshith Reddy <deekshithreddychilakala@gmail.com>
2025-01-09 23:30:14 +0530

    Investment calculator app as part of section 4: time to practice

---
+++ section-3-angular-essentials-time-to-practice/src/app/investment-input.model.ts
@@ -0,0 +1,2 @@
+export interface InvestmentInput { initialInvestment: number; duration: number; expectedReturn: number; annualInvestment: number }
+// export type InvestmentInput ={ initialInvestment: number; duration: number; expectedReturn: number; annualInvestment: number }
