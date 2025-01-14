868fcd8e15df9f3ebbfd805022ac1a24c4ca1610
Chilakala Venkata Deekshith Reddy <deekshithreddychilakala@gmail.com>
2025-01-14 16:41:33 +0530

    section 14 - Code Splitting and Deferrable Views - Route based Lazy Loading

---
+++ section-15-code-splitting-and-deferrable-views/src/app/users/users.routes.ts
@@ -0,0 +1,39 @@
+import { ResolveFn, Routes } from '@angular/router';
+
+import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
+import { inject } from '@angular/core';
+import { Task } from '../tasks/task/task.model';
+import { TasksService } from '../tasks/tasks.service';
+import { resolveUserTasks, TasksComponent } from '../tasks/tasks.component';
+
+
+
+export const routes: Routes = [
+  {
+    path: '',
+    providers: [
+      TasksService
+    ],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+        // loadComponent: () => import('../tasks/tasks.component').then((mod) => mod.TasksComponent),
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage]
+      },
+    ]
+  }
+];
