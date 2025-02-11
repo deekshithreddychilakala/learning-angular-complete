6ef7a1d7b63d0df7d3be51c30c7d0b62cecf026e
Chilakala Venkata Deekshith Reddy <deekshithreddychilakala@gmail.com>
2025-02-12 00:38:47 +0530

    section 41 - NgRx

---
+++ section-41-ngrx/src/app/counter-output/counter-output.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+
+import { Store } from '@ngrx/store';
+import { selectCount, selectDouble } from '../store/counter.selectors';
+
+@Component({
+  selector: 'app-counter-output',
+  templateUrl: './counter-output.component.html',
+  styleUrls: ['./counter-output.component.css'],
+})
+export class CounterOutputComponent {
+  count$: Observable<number>;
+  doubleCount$: Observable<number>;
+
+  constructor(private store: Store<{ counter: number }>) {
+    this.count$ = store.select(selectCount);
+    this.doubleCount$ = store.select(selectDouble);
+  }
+
+}
